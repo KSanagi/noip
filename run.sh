0a81523cf44dcb4f6852a5698110ad45ee584155
Koki Sanagi <sanagikoki@gmail.com>
2017-03-11 20:40:26 +0000

    correct extracted dir name

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+yum -y install gcc
 wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz
 tar xzf noip-duc-linux.tar.gz
-cd no-ip-2.1.9
+cd no-ip-2.1.9-1
 make
 make install
+rm -rf no-ip-2.1.9-1 no-ip-2.1.9.tar.gz
